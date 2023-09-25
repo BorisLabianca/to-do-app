2d77ca26c785daddd03ca02150f9e2271316fea0
Boris Labianca <boris.labianca@hotmail.fr>
2023-09-25 16:13:43 +0800

    created DB connection and created task schema

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
   res.status(200).send("Home page");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Started On Port ${process.env.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Started On Port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
