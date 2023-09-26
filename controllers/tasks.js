f7e038b392311850164011b1a0dcac89adff6f0e
Boris Labianca <boris.labianca@hotmail.fr>
2023-09-26 11:18:06 +0800

    added get single task route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,4 +18,15 @@ const getTasks = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks };
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if (!task) return res.status(404).json(`No task found with id ${id}`);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createTask, getTasks, getTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const Task = require("../models/Task");
-const { createTask, getTasks } = require("../controllers/tasks");
+const { createTask, getTasks, getTask } = require("../controllers/tasks");
 const router = express.Router();
 
 router.post("/api/add-tasks", createTask);
 
 router.get("/api/all-tasks", getTasks);
+router.get("/api/all-tasks/single-Task/:id", getTask);
 
 module.exports = router;
